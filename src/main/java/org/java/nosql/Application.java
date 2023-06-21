a1aa0c405ab55daf6eb7d9a4342cb4cc9b684e15
yushu <1036221163@qq.com>
2023-06-21 16:28:43 +0800

    feat:add kafka

--- src/main/java/org/java/nosql/Application.java
+++ src/main/java/org/java/nosql/Application.java
@@ -1,8 +1,13 @@
 package org.java.nosql;
 
 //import org.mybatis.spring.annotation.MapperScan;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
 //@MapperScan("org.java.aop.dao.mapper")
 @SpringBootApplication
 public class Application {
@@ -10,4 +15,18 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println(in);
+    }
+
 }
